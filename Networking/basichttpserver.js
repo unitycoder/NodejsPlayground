30c08c0e6bcea40f592ee8af73a87c46d78a3e89
mika <unitycoder@users.noreply.github.com>
2016-06-14 15:03:17 +0800

    Create basichttpserver.js

---
+++ Networking/basichttpserver.js
@@ -0,0 +1,33 @@
+// basic HTTP server
+// https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
+
+var http = require("http");
+var PORT = 7777;
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  var headers = request.headers;
+  var httpVersion = request.httpVersion;
+  var rawHeaders = request.rawHeaders;
+  var method = request.method;
+  var rawTrailers = request.rawTrailers;
+  var statusCode = request.statusCode;
+  var statusMessage = request.statusMessage;
+  var socket = request.socket;
+  var trailers = request.trailers;
+  var url = request.url;
+
+  response.write("headers:"+headers);
+  response.write("\n\nhttpVersion:"+httpVersion);
+  response.write("\n\nrawHeaders:"+rawHeaders);
+  response.write("\n\nmethod:"+method);
+  response.write("\n\nrawTrailers:"+rawTrailers);
+  response.write("\n\nstatusCode:"+statusCode);
+  response.write("\n\nstatusMessage:"+statusMessage);
+  response.write("\n\nsocket:"+socket);
+  response.write("\n\ntrailers:"+trailers);
+  response.write("\n\nurl:"+url);
+
+  response.end();
+}).listen(PORT);
